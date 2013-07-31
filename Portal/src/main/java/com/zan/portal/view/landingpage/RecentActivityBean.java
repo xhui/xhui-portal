897b7c726e40b441d1bfe1b916b00bbf9ae2f2c7
xhui.zhou@gmail.com <xhui.zhou@gmail.com>
2013-07-31 22:44:10 +0800

    Change to basic slider

--- Portal/src/main/java/com/zan/portal/view/landingpage/RecentActivityBean.java
+++ Portal/src/main/java/com/zan/portal/view/landingpage/RecentActivityBean.java
@@ -15,42 +15,39 @@ import com.zan.portal.view.base.AppBean;
 
 @Component
 @Scope("request")
-public class RecentActivityBean implements Serializable
-{
-	private static final long		serialVersionUID	= -4733669591503939365L;
+public class RecentActivityBean implements Serializable {
+	private static final long serialVersionUID = -4733669591503939365L;
 
 	@Inject
-	private AppBean					appBean;
+	private AppBean appBean;
 
-	private List<RecentActivity>	activities;
+	private List<RecentActivity> activities;
 
 	@PostConstruct
-	private void init()
-	{
+	private void init() {
 		activities = new ArrayList<RecentActivity>();
 
 		String imgUrlPrefix = appBean.getResourcePath() + "/images/";
 		RecentActivity act = new RecentActivity();
-		act.setImgUrl(imgUrlPrefix + "slider-image1.jpg");
+		act.setImgUrl("http://farm3.staticflickr.com/2832/9395296803_eb3ba90a5a_z.jpg");
 		act.setDesc("This is a test 1.");
 		act.setLinkUrl("#");
 		activities.add(act);
 
 		act = new RecentActivity();
-		act.setImgUrl(imgUrlPrefix + "slider-image2.jpg");
+		act.setImgUrl("http://farm3.staticflickr.com/2836/9398047998_93151bdbdb_z.jpg");
 		act.setDesc("This is a test 2.");
 		act.setLinkUrl("#");
 		activities.add(act);
 
 		act = new RecentActivity();
-		act.setImgUrl(imgUrlPrefix + "slider-image3.jpg");
+		act.setImgUrl("http://farm8.staticflickr.com/7402/9395282679_984948d129_z.jpg");
 		act.setDesc("This is a test 3.");
 		act.setLinkUrl("#");
 		activities.add(act);
 	}
 
-	public List<RecentActivity> getActivities()
-	{
+	public List<RecentActivity> getActivities() {
 		return activities;
 	}
 }
